c8ff646626893ba456809242097412cc5a073527
codewithumarrr <94783559+codewithumarrr@users.noreply.github.com>
2023-10-03 17:01:47 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Dashboard from "./features/dashboard/dashboard";
+import Login from "./features/login/login";
+
+function App() {
+  return (
+    <>
+      {/* <Login /> */}
+      <Dashboard />
+    </>
+  );
+}
+
+export default App;
