d112fcdd2fb7addf6721088b897759deddf5dc42
Attaurrehman-bop <73884338+themuhammadattaurrehman@users.noreply.github.com>
2023-10-04 12:29:42 +0500

    put two pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Dashboard from "./features/dashboard/dashboard";
 import Login from "./features/login/login";
-
+import Cry from "./features/Cya/cya";
+import Evc from "./features/Evc/Evc";
 function App() {
   return (
     <>
+    <Evc />
+    {/* <Cry /> */}
       {/* <Login /> */}
-      <Dashboard />
+      {/* <Dashboard /> */}
     </>
   );
 }
