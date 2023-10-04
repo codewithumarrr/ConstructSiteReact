8bd376e8e07e21e5f27029d5210a4cf462d015c2
Attaurrehman-bop <73884338+themuhammadattaurrehman@users.noreply.github.com>
2023-10-04 13:28:57 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import Dashboard from "./features/dashboard/dashboard";
 import Login from "./features/login/login";
 import Cry from "./features/Cya/cya";
 import Evc from "./features/Evc/Evc";
+import Rp from "./features/Rp/rp";
 function App() {
   return (
     <>
-    <Evc />
+    
+    {/* <Evc /> */}
     {/* <Cry /> */}
       {/* <Login /> */}
       {/* <Dashboard /> */}
+      <Rp />
     </>
   );
 }
